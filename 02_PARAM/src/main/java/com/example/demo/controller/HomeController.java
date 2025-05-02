9af7e9212977889a5be78ccfb4f2f7fce022e68a
yunsubin <tasb01236@daum.net>
2025-05-02 12:37:05 +0900

    01

---
+++ 02_PARAM/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @GetMapping(value = "/")
+    public String home(){
+
+        log.info("GET /");
+        return "index";
+    }
+}
